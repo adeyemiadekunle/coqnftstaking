bb6a865e6386caa2446d3594b06554804b554a43
adeyemiadekunle <adeyemisuraju15@gmail.com>
2024-03-07 18:21:14 +0100

    upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xF28B76206DE1F156F28A745c22231CC9165Ae588";
+export const tokenContractAddress =
+  "0x9E42f9f1d08Bd32C7eF4Bcfce2E1F29EC4951b3F";
+export const stakingContractAddress =
+  "0x8655A6ceD7cE6f2B13f7E791adE7213f86429a88";
